ff13f53d8eb11c9b27c4f480b8c2ae1df5ecb0f5
Aly Yakan <alyyakan@hotmail.com>
2015-08-19 14:37:55 +0300

    comment in seperate html component and delete ajax

--- static/js/delete.js
+++ static/js/delete.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-	
-})
+	$('.comment-delete-btn').click(function(event) {
+		event.preventDefault();
+		var id = $(this).attr('data-id');
+
+		$.ajax({
+            type: "GET",
+            url: $('.comment-delete-form').attr('action'),
+            data: {'id': id, 'csrfmiddlewaretoken' : $("#csrf").attr('value')},
+            success: function(data){
+            	if(JSON.parse(data).success == 1) {
+            		$('#comment-div-' + id).remove() 
+            	}
+            },
+            dataType: 'html'
+        });
+	});
+});
