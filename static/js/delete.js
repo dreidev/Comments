411e0f5da31067d60c71ddab7eb5514408f71a47
ranael-garem <ranaelgarem781994@gmail.com>
2015-08-23 08:53:45 +0200

    added confirm to delete

--- static/js/delete.js
+++ static/js/delete.js
@@ -2,17 +2,21 @@ $(document).ready(function() {
 	$('.comment-delete-btn').click(function(event) {
 		event.preventDefault();
 		var id = $(this).attr('data-id');
-
-		$.ajax({
-            type: "GET",
-            url: $('.comment-delete-form').attr('action'),
-            data: {'id': id, 'csrfmiddlewaretoken' : $("#csrf").attr('value')},
-            success: function(data){
-            	if(JSON.parse(data).success == 1) {
-            		$('#comment-div-' + id).remove() 
-            	}
-            },
-            dataType: 'html'
-        });
-	});
+            if(confirm("Are you sure you want to delete this comment?")){
+      		$.ajax({
+                  type: "GET",
+                  url: $('.comment-delete-form').attr('action'),
+                  data: {'id': id, 'csrfmiddlewaretoken' : $("#csrf").attr('value')},
+                  success: function(data){
+                  	if(JSON.parse(data).success == 1) {
+                  		$('#comment-div-' + id).remove() 
+                  	}
+                        else {
+                              alert("You don't have permission to delete this comment")
+                        }
+                  },
+                  dataType: 'html'
+              });
+            }
+      });
 });
