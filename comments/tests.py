6ef69843be4d6e24575ceb87d5bbab81b30aed72
Aly Yakan <alyyakan@hotmail.com>
2015-08-24 09:42:34 +0300

    #5 Added tests and documentation

--- comments/tests.py
+++ comments/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
-from .models import Comment
+from django.test import TestCase, Client
+from .models import Comment, Like
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 
 class AddCommentTestCase(TestCase):
@@ -37,3 +38,69 @@ class DeleteCommentTestCase(TestCase):
             HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Comment.objects.all().count(), init_comment_count)
+
+
+class LikeCommentTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        User.objects.create_user(username='johndoe', password='123456')
+        self.client.login(username='johndoe', password='123456')
+
+    def test_like_comment(self):
+        """
+        Tests that an authenticated user can like a comment.
+        Author: Aly Yakan
+        """
+        self.client.post(
+            reverse('comment-create'),
+            {'comment': 'form'},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        init_likes_count = Like.objects.all().count()
+        response = self.client.get(
+            reverse('comment-like'),
+            {'comment_id': 1},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Like.objects.all().count(), init_likes_count+1)
+
+    def test_unlike_comment(self):
+        """
+        Tests that an authenticated user can unlike a comment he liked
+        Author: Aly Yakan
+        """
+        self.client.post(
+            reverse('comment-create'),
+            {'comment': 'form'},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        init_likes_count = Like.objects.all().count()
+        response = self.client.get(
+            reverse('comment-like'),
+            {'comment_id': 1},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Like.objects.all().count(), init_likes_count+1)
+        response = self.client.get(
+            reverse('comment-unlike'),
+            {'comment_id': 1},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Like.objects.all().count(), init_likes_count)
+
+    def test_like_comment_not_authenticated(self):
+        """
+        Tests that a guest (not authenticated) cannot like a comment
+        Author: Aly Yakan
+        """
+        self.client.logout()
+        self.client.post(
+            reverse('comment-create'),
+            {'comment': 'form'},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        init_likes_count = Like.objects.all().count()
+        response = self.client.get(
+            reverse('comment-like'),
+            {'comment_id': 1},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Like.objects.all().count(), init_likes_count+0)
