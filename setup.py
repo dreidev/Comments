7edf3dab5f23f95b093035c88cc8350e9151d065
Aly Yakan <alyyakan@hotmail.com>
2015-08-25 12:22:07 +0300

    Packaging

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-comments',
+    version='1.0.0.dev1',
+    author=u'DREIDEV',
+    author_email='info@dreidev.com',
+    url='https://github.com/alyakan/Comments',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+         'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='comments comment development',
+    description='Associates comments functionality with any given model',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+)
