81a00cc04f005f1b619975bd4a9fe685772a2338
Aly Yakan <alyyakan@hotmail.com>
2015-08-25 14:01:26 +0300

    Modified version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-ajax-comments',
-    version='1.0.1.dev1',
+    version='1.0.2.dev1',
     author=u'DREIDEV',
     author_email='info@dreidev.com',
     url='https://github.com/dreidev/Comments',
