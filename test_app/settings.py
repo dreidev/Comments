5c3dae5c9669aeb4869d975d383374708732d8da
Aly Yakan <alyyakan@hotmail.com>
2015-08-27 13:14:09 +0300

    coveralls

---
+++ test_app/settings.py
@@ -0,0 +1,31 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db',
+    }
+}
+USE_TZ = False
+SITE_ID = 1
+SECRET_KEY = '{{secret_key}}'
+
+ROOT_URLCONF = 'test_app.urls'
+STATIC_URL = '/static/'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_nose',
+    'south',
+    'comments',
+)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = ('--nocapture', )
